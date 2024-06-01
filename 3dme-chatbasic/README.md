475eeae4b69f37e54d34128316ab11d990559112
Yxukia <137515645+yxukiaofficial@users.noreply.github.com>
2024-05-31 22:44:03 -0700

    Add files via upload

---
+++ 3dme-chatbasic/README.md
@@ -0,0 +1,10 @@
+# 3dme--w--html
+
+I got bored so I made a /me, /ooc, /do, and /med script
+
+To create it myself.
+
+![IMG1 ME](https://images.guns.lol/x0vUH.png)
+![IMG2 DO](https://images.guns.lol/TUjv5.png)
+![IMG3 MED](https://images.guns.lol/No4Am.png)
+![IMG3 OOC](https://images.guns.lol/OgZNI.png)
---
+++ 3dme-chatbasic/fxmanifest.lua
@@ -0,0 +1,32 @@
+--- Just a test
+--- Feel free to contact me on Instagram @yxukia for help
+
+resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9' 
+
+fx_version 'bodacious'
+
+game 'gta5'
+
+author 'Yxukia'
+
+version '1.0.3'
+
+ui_page "html/index.html"
+
+
+shared_scripts {
+	'config.lua'
+}
+
+client_scripts {
+ 'client.lua'
+}
+
+server_scripts {
+ 'server.lua'
+}
+
+
+files {
+ 'html/*',
+}
