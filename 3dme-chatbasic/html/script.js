6378283c2d9fb723072290c85d71a04f56f17bf4
Yxukia <137515645+yxukiaofficial@users.noreply.github.com>
2024-05-31 22:45:02 -0700

    Delete 3dme-chatbasic directory

--- 3dme-chatbasic/html/script.js
+++
@@ -1,8 +0,0 @@
-addEventListener("message", function(event){
-	if (event.data.toggle == true) {
-		document.getElementById("data").innerHTML = event.data.html;
-        $("#data").show()
-    }else{
-    	$("#data").hide()
-    }
-});
